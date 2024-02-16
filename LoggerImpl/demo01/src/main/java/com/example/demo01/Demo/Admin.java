fb94e994c0715befa20db93acd1a605662062278
132905 <125987008+132905@users.noreply.github.com>
2024-02-16 13:25:56 +0530

    Logger Implementation

---
+++ LoggerImpl/demo01/src/main/java/com/example/demo01/Demo/Admin.java
@@ -0,0 +1,30 @@
+package com.example.demo01.Demo;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String usermail;
+    private String userpassword;
+
+
+
+
+//    public Admin(Long id, String username, String useremail) {
+//
+//    }
+}
