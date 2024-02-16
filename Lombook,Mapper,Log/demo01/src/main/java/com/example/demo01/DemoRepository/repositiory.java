e05de0e84982d33db563ccb9437f73983287f409
132905 <125987008+132905@users.noreply.github.com>
2024-02-16 13:24:13 +0530

    Delete Lombook,Mapper,Log/demo01 directory

--- Lombook,Mapper,Log/demo01/src/main/java/com/example/demo01/DemoRepository/repositiory.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo01.DemoRepository;
-
-import com.example.demo01.Demo.Admin;
-import com.example.demo01.Demo.UserDTo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface repositiory extends JpaRepository<Admin,Long> {
-}
